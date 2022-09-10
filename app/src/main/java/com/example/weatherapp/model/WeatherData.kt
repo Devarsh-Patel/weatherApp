faf1f8ca12e86954939e46c84ed43d6f9e10c743
Consultant <consultant@Admins-MacBook-Pro-5.local>
2022-09-09 23:51:24 -0400

    added the reference type trnasfer weatherData

--- app/src/main/java/com/example/weatherapp/model/WeatherData.kt
+++ app/src/main/java/com/example/weatherapp/model/WeatherData.kt
@@ -1,7 +1,9 @@
 package com.example.weatherapp.model
 
+import java.io.Serializable
+
 data class WeatherData(
     var cities: List<City?>?,
     var startIndex: Int?, // 0
     var totalCitiesFound: Int? // 41
-)
+):Serializable
